d797498de036554722046ebceafa2e4c9aeeeed9
chris-ortiz <christian.ortiz@adesso.de>
2024-12-06 23:37:10 +0100

    day 5 - 2

--- day5.exs
+++ day5.exs
@@ -7,9 +7,12 @@ defmodule Day5 do
         Map.merge(acc, %{a => [b]}, fn k, v1, _ -> [b | v1] end)
       end)
 
-    sum =
+    reports =
       String.split(reports, "\n")
       |> Enum.map(fn line -> String.split(line, ",") end)
+
+    sum =
+      reports
       |> Enum.map(fn report -> extract_middle(order, report) end)
       |> Enum.filter(fn v ->
         case v do
@@ -23,7 +26,35 @@ defmodule Day5 do
       end)
       |> Enum.sum()
 
-    IO.inspect(sum)
+    IO.puts("Question 1: " <> Integer.to_string(sum))
+
+    invalid_reports =
+      reports
+      |> Enum.filter(fn report ->
+        index_map = Enum.with_index(report) |> Map.new()
+        !is_report_valid?(order, index_map, report)
+      end)
+      |> Enum.map(fn report -> reorder(report, order) end)
+      |> Enum.map(fn report -> Enum.at(report, floor(length(report) / 2)) end)
+      |> Enum.map(&Integer.parse/1)
+      |> Enum.map(fn {i, _} -> i end)
+      |> Enum.sum()
+
+    IO.puts("Question 2: " <> Integer.to_string(invalid_reports))
+  end
+
+  def reorder(report, order) do
+    report
+    |> Enum.sort(fn a, b ->
+      index_map = Enum.with_index(report) |> Map.new()
+      items_after_b = Map.get(order, b)
+
+      if Enum.member?(items_after_b, a) do
+        false
+      else
+        true
+      end
+    end)
   end
 
   def extract_middle(order, report) do
